4785269501d9cfbe6482cf0df33b249992f903d3
istommao <istommao@gmail.com>
2017-01-24 10:26:33 +0800

    Modify server.py

--- feeder/server.py
+++ feeder/server.py
@@ -1,4 +1,6 @@
 """feeder server.py."""
+import optparse
+
 from flask import Flask, render_template, abort
 
 import feedparser
@@ -69,5 +71,46 @@ def infoq_subscription(urlname):
     return render_template('content.html', data=feed, sidemenus=sidemenus)
 
 
+def flaskrun(default_host="127.0.0.1", default_port="5000"):
+    """
+    Takes a flask.Flask instance and runs it. Parses
+    command-line flags to configure the app.
+    """
+
+    # Set up the command-line options
+    parser = optparse.OptionParser()
+    parser.add_option("-H", "--host",
+                      help="Hostname of the Flask app [default %s]" % default_host,
+                      default=default_host)
+    parser.add_option("-P", "--port",
+                      help="Port for the Flask app [default %s]" % default_port,
+                      default=default_port)
+
+    # Two options useful for debugging purposes, but
+    # a bit dangerous so not exposed in the help message.
+    parser.add_option("-d", "--debug",
+                      action="store_true", dest="debug",
+                      help=optparse.SUPPRESS_HELP)
+    parser.add_option("-p", "--profile",
+                      action="store_true", dest="profile",
+                      help=optparse.SUPPRESS_HELP)
+
+    options, _ = parser.parse_args()
+
+    # If the user selects the profiling option, then we need
+    # to do a little extra setup
+    if options.profile:
+        from werkzeug.contrib.profiler import ProfilerMiddleware
+
+        APP.config['PROFILE'] = True
+        APP.wsgi_app = ProfilerMiddleware(APP.wsgi_app, restrictions=[30])
+        options.debug = True
+
+    APP.run(
+        debug=options.debug,
+        host=options.host,
+        port=int(options.port)
+    )
+
 if __name__ == '__main__':
-    APP.run(debug=True)
+    flaskrun()
